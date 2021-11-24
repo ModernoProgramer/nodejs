a8c3b689613e147a7ed434ac5630895ad8ba8c1d
Moderno <83356984+ModernoProgramer@users.noreply.github.com>
2021-11-23 21:43:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.get("/", function(req, res) { // index
   res.send("HELLO WORLD");
 })
 
+
 let port = 8000
-app.listen(port, function() {
-  console.log("Server Listen in: " + port);
+app.listen(port, () => {
+  console.log("Server Listen in: http://localhost:" + port);             
 });
